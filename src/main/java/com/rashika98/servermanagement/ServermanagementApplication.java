23090cdf793613297b9193462e10a84cad3f8082
Rashika1998 <rashikamr1998@gmail.com>
2022-01-13 09:58:06 +0530

    Server management application.

---
+++ src/main/java/com/rashika98/servermanagement/ServermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.rashika98.servermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServermanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rashika98/servermanagement/ServermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rashika98.servermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
